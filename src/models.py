70edd515880cf35abcde9fd7e151f59500f0ad1c
Janio Cesar <cesargarcia718@gmail.com>
2020-08-24 23:14:51 +0000

    Added classes to models.py

--- src/models.py
+++ src/models.py
@@ -39,3 +39,94 @@ class Profile(db.Model):
             
             # do not serialize the password, its a security breach
         }
+
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    diet = db.Column(db.String(120), unique=False, nullable=False)
+    address = db.Column(db.String(120), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=True)
+    phone_number = db.Column(db.String(120),  nullable=False)
+    operational_hours = db.Column(db.String(120),  nullable=True)
+    pricing = db.Column(db.String(120),  nullable=True)
+
+    def __repr__(self):
+        return '<Profile %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "name": self.name,
+            "phone_number": self.phone_number,
+            "diet": self.diet,
+            
+            # do not serialize the password, its a security breach
+        }
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    diet = db.Column(db.String(120), unique=False, nullable=False)
+    address = db.Column(db.String(120), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=True)
+    phone_number = db.Column(db.String(120),  nullable=False)
+    operational_hours = db.Column(db.String(120),  nullable=True)
+    pricing = db.Column(db.String(120),  nullable=True)
+
+    def __repr__(self):
+        return '<Profile %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "name": self.name,
+            "phone_number": self.phone_number,
+            "diet": self.diet,
+            
+            # do not serialize the password, its a security breach
+        }
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    diet = db.Column(db.String(120), unique=False, nullable=False)
+    address = db.Column(db.String(120), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=True)
+    phone_number = db.Column(db.String(120),  nullable=False)
+    operational_hours = db.Column(db.String(120),  nullable=True)
+    pricing = db.Column(db.String(120),  nullable=True)
+
+    def __repr__(self):
+        return '<Profile %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "name": self.name,
+            "phone_number": self.phone_number,
+            "diet": self.diet,
+            
+            # do not serialize the password, its a security breach
+        }
+
+class Review_picture(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.String(120), unique=True, nullable=False)
+    review_id = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Profile %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.image_url,
+            "name": self.review_id,
+            "phone_number": self.phone_number,
+            "diet": self.diet,
+            
+            # do not serialize the password, its a security breach
+        }
